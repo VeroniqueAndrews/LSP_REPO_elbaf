2bcdfef46e8b88f76fcd65eac6ae5c02a9c608e6
veronique8 <veronique.andrews@bison.howard.edu>
2024-09-03 22:48:58 -0400

    intial commit create array for words

---
+++ large-scale-programming/org/howard/edu/lsp/assignment2/WordCounting.java
@@ -0,0 +1,16 @@
+package org.howard.edu.lsp.assignment2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class WordCounting {
+    public static void main(String[] args) {
+        String wordsPath = "words.txt";
+
+        ArrayList<String> wordStrings = new ArrayList<>()
+    }
+
+}
+ 
