d62933ee6b9e778433b0f0c3a104fd0c29357403
veronique8 <veronique.andrews@bison.howard.edu>
2024-10-17 19:33:54 -0400

    attributes of Book

---
+++ large-scale-programming/org/howard/edu/lsp/midterm/question1/Book.java
@@ -0,0 +1,9 @@
+public class Book {
+
+    //The Book class has the following attributes:
+    private String title
+    private String author
+    private String ISBN
+    pirvaye int yearPublished
+
+}
